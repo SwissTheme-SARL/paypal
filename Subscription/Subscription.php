0ffe05d30e10b1e9e0750317ec41ee64d8f74940
Gilles Hemmerlé <giloux@gmail.com>
2020-07-20 02:11:06 +0200

    Fixed default url for paypal back

--- Subscription/Subscription.php
+++ Subscription/Subscription.php
@@ -1,45 +1,92 @@
 <?php
 
-
 namespace SwissthemeSarl\paypal\Subscription;
 
 use App\Entity\Items;
-
 use App\Entity\User;
-use Symfony\Component\Security\Core\Security;
 
 class Subscription
 {
-    public function setPlanPayload(Items $item){
-        $payload = [
+    public function setPlanPayload(Items $item)
+    {
+        return [
             'product_id' => $item->getPaypalProduct(),
             'name' => $item->getType(),
             'description' => $item->getType(),
             'status' => 'ACTIVE',
             'billing_cycles' => [
-                self::setFrequency($item)
+                self::setFrequency($item),
             ],
             'payment_preferences' => [
                 'auto_bill_outstanding' => true,
                 'setup_fee' => [
                     'value' => $item->getPrice(),
-                    'currency_code' => 'CHF'
+                    'currency_code' => 'CHF',
                 ],
                 'setup_fee_failure_action' => 'CONTINUE',
-                'payment_failure_threshold' => 3
+                'payment_failure_threshold' => 3,
             ],
             'taxes' => [
                 'percentage' => 7,
-                'inclusive' => true
-            ]
+                'inclusive' => true,
+            ],
         ];
+    }
+
+    public function setPlanHeaders($token)
+    {
+        return [
+            'Content-Type' => 'application/json',
+            'Authorization' => $token,
+        ];
+    }
+
+    public function setSubscriptionPayLoad(string $plan, Items $item, User $user)
+    {
+        $payload = [
+            'plan_id' => $plan,
+            //'start_time' => new \DateTime('now'),
+            'quantity' => 1,
+            'shipping_amount' => [
+                'currency_code' => 'CHF',
+                'value' => $item->getPrice(),
+            ],
+            'email_address' => $user->getEmail(),
+            'shipping_address' => [
+                'full_name' => 'parentsolo',
+            ],
+            'application_context' => [
+                'brand_name' => 'parentsolo',
+                'user_action' => 'SUBSCRIBE_NOW',
+                'payment_method' => [
+                    'payer_selected' => 'PAYPAL',
+                    'payee_preferred' => 'IMMEDIATE_PAYMENT_REQUIRED',
+                ],
+                'return_url' => empty($_ENV['PAYPAL_RETURN_URL']) ? 'https://parentsolo.ch/paypal/accept/sub' : $_ENV['PAYPAL_RETURN_URL'],
+                'cancel_url' => empty($_ENV['PAYPAL_CANCEL_URL']) ? 'https://parentsolo.ch/paypal/cancel/sub' : $_ENV['PAYPAL_CANCEL_URL'],
+            ],
+        ];
+
         return $payload;
     }
 
-    private function setFrequency(Items $item){
-        $object = array('frequency' => [
+    public function capturePayload($details)
+    {
+        return $payload = [
+            'note' => 'first payment for subscription',
+            'capture_type' => 'OUTSTANDING_BALANCE',
+            'amount' => [
+                'currency_code' => $details->shipping_amount->currency_code,
+                'value' => $details->shipping_amount->value,
+            ],
+        ];
+    }
+
+    private function setFrequency(Items $item)
+    {
+        return ['frequency' => [
             'interval_unit' => 'MONTH',
-            'interval_count' => $item->getDuration()
+            'interval_count' => $item->getDuration(),
         ],
             'tenure_type' => 'REGULAR',
             'sequence' => 1,
@@ -47,62 +94,12 @@ class Subscription
             'pricing_scheme' => [
                 'fixed_price' => [
                     'value' => $item->getPrice(),
-                    'currency_code' => 'CHF'
-                ]
-        ]);
-        return $object;
-    }
-
-    public function setPlanHeaders($token){
-        $headers = [
-            'Content-Type' => 'application/json',
-            'Authorization' => $token
-        ];
-        return $headers;
-    }
-
-    public function setSubscriptionPayLoad(string $plan, Items $item, User $user){
-        $payload = [
-            'plan_id' => $plan,
-            //'start_time' => new \DateTime('now'),
-            'quantity' => 1,
-            'shipping_amount' => [
-                'currency_code' => 'CHF',
-                'value' => $item->getPrice()
-            ],
-            'email_address' => $user->getEmail(),
-            'shipping_address' => [
-                'full_name' => 'parentsolo'
-            ],
-            'application_context' => [
-                'brand_name' => 'parentsolo',
-                'user_action' => 'SUBSCRIBE_NOW',
-                'payment_method' => [
-                    'payer_selected' => 'PAYPAL',
-                    'payee_preferred' => 'IMMEDIATE_PAYMENT_REQUIRED'
+                    'currency_code' => 'CHF',
                 ],
-                'return_url' => 'https://parentsolo.ch/paypal/accept/sub',
-                'cancel_url' => 'https://parentsolo.ch/paypal/cancel/sub'
-            ]
-        ];
-        return $payload;
+            ], ];
     }
 
-    public function capturePayload($details){
-        return $payload = [
-            'note' => 'first payment for subscription',
-            'capture_type' => 'OUTSTANDING_BALANCE',
-            'amount' => [
-                'currency_code' => $details->shipping_amount->currency_code,
-                'value' => $details->shipping_amount->value
-            ]
-        ];
-    }
-
-
-
-
-//{
+    //{
 //"plan_id": "P-5ML4271244454362WXNWU5NQ",
 //"start_time": "2018-11-01T00:00:00Z",
 //"quantity": "20",
@@ -143,5 +140,4 @@ class Subscription
 //    "cancel_url": "https://example.com/cancelUrl"
 //  }
 //}
-
 }
